48aabfb8bc8dae1e8596e6c4ea5919a3f6a0b3c7
Zack P <zackjp87@gmail.com>
2025-09-16 21:39:23 -0400

    [Home] Apply small rounded corners to ItemDisplay

--- app/src/main/java/com/example/fakeamazon/features/home/component/ItemDisplay.kt
+++ app/src/main/java/com/example/fakeamazon/features/home/component/ItemDisplay.kt
@@ -4,10 +4,12 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
@@ -32,6 +34,7 @@ fun ItemDisplay(
 ) {
     ConstraintLayout(
         modifier = modifier
+            .clip(RoundedCornerShape(4.dp))
             .background(RECOMMENDED_ITEM_BG_COLOR)
             .padding(dimensionResource(R.dimen.padding_xxsmall))
     ) {
