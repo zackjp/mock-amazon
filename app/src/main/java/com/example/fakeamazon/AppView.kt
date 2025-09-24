499111a36ee89430c2b7f2a9540d9069a2a927b9
Zack P <zackjp87@gmail.com>
2025-09-24 17:32:54 -0400

    [Home] Make Navigation Chips scroll out of view like main content, but animate back in when overscrolled

--- app/src/main/java/com/example/fakeamazon/AppView.kt
+++ app/src/main/java/com/example/fakeamazon/AppView.kt
@@ -1,8 +1,12 @@
 package com.example.fakeamazon
 
+import androidx.annotation.FloatRange
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
@@ -31,21 +35,38 @@ import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
+import androidx.compose.ui.input.nestedscroll.NestedScrollSource
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.Velocity
 import androidx.compose.ui.unit.dp
 import com.example.fakeamazon.features.home.HomeScreenRoot
 import com.example.fakeamazon.ui.theme.FakeAmazonTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun App() {
     val bottomNavItems = listOf(
@@ -55,13 +76,21 @@ fun App() {
         BottomNavItem(Icons.Outlined.Menu),
     )
 
+    var navChipsHeightPx by remember { mutableFloatStateOf(0f) }
+    val collapsibleState = rememberCollapsibleState(maxCollapseHeightPx = -navChipsHeightPx)
+
     Scaffold(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .nestedScroll(collapsibleState.scrollObserver),
         topBar = {
             AmazonTopAppBar(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = dimensionResource(R.dimen.padding_small)),
+                modifier = Modifier.fillMaxWidth(),
+                navChipsOffset = collapsibleState.currentOffsetPx.value,
+                offsetFraction = collapsibleState.offsetFraction.value,
+                onNavChipsSizeChange = { intSize ->
+                    navChipsHeightPx = intSize.height.toFloat()
+                },
             )
         },
         bottomBar = {
@@ -80,7 +109,12 @@ fun App() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-private fun AmazonTopAppBar(modifier: Modifier = Modifier) {
+private fun AmazonTopAppBar(
+    modifier: Modifier = Modifier,
+    navChipsOffset: Float,
+    @FloatRange(0.0, 1.0) offsetFraction: Float,
+    onNavChipsSizeChange: (IntSize) -> Unit,
+) {
     val paddingMedium = dimensionResource(R.dimen.padding_medium)
     val paddingLarge = dimensionResource(R.dimen.padding_large)
 
@@ -99,19 +133,45 @@ private fun AmazonTopAppBar(modifier: Modifier = Modifier) {
         "Customer Service"
     )
 
-    Column(modifier = modifier) {
-        SimpleSearchBar(
+    val topBarBackgroundColorStart = Color.White.copy(alpha = offsetFraction)
+    val topBarBackgroundColorEnd = Color.White.copy(alpha = .75f * offsetFraction)
+
+    Box(modifier = modifier) {
+        Box(
             modifier = Modifier
-                .padding(
-                    start = paddingLarge,
-                    end = paddingLarge,
+                .matchParentSize()
+                .graphicsLayer {
+                    translationY = navChipsOffset
+                }
+                .background(
+                    Brush.verticalGradient(
+                        0f to topBarBackgroundColorStart,
+                        .45f to topBarBackgroundColorStart,
+                        .7f to topBarBackgroundColorEnd
+                    )
                 )
         )
 
-        NavChipsRow(
-            modifier = Modifier.padding(top = paddingMedium),
-            navigationChips = navChips,
-        )
+        Column(modifier = Modifier.padding(bottom = 8.dp)) {
+            SimpleSearchBar(
+                modifier = Modifier
+                    .padding(
+                        start = paddingLarge,
+                        end = paddingLarge,
+                    )
+            )
+
+            NavChipsRow(
+                modifier = Modifier
+                    .onSizeChanged(onNavChipsSizeChange)
+                    .padding(top = paddingMedium)
+                    .graphicsLayer {
+                        alpha = (1 - offsetFraction)
+                        translationY = navChipsOffset
+                    },
+                navigationChips = navChips,
+            )
+        }
     }
 }
 
@@ -228,6 +288,17 @@ private fun NavChipsRow(modifier: Modifier, navigationChips: List<String>) {
     }
 }
 
+@Composable
+private fun rememberCollapsibleState(maxCollapseHeightPx: Float): CollapsibleState {
+    val coroutineScope = rememberCoroutineScope()
+    return remember(maxCollapseHeightPx, coroutineScope) {
+        CollapsibleState(
+            coroutineScope = coroutineScope,
+            maxCollapseHeight = maxCollapseHeightPx,
+        )
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun AppPreview() {
@@ -239,3 +310,72 @@ fun AppPreview() {
 private data class BottomNavItem(
     val icon: ImageVector
 )
+
+private class CollapsibleState(
+    val coroutineScope: CoroutineScope,
+    val maxCollapseHeight: Float,
+) {
+
+    val currentOffsetPx = Animatable(0f)
+    val offsetFraction = Animatable(0f)
+
+    val scrollObserver =
+        object : NestedScrollConnection {
+            override fun onPostScroll(
+                consumed: Offset,
+                available: Offset,
+                source: NestedScrollSource
+            ): Offset {
+                val isScrollingAwayFromTop = consumed.y < 0.0f
+                val isOverscrollingAtTop = consumed.y == 0.0f && available.y > 0.0f
+                val deltaToApply: Float? = when {
+                    // offset the collapsible view by the same amount that main content
+                    // is scrolling away from the top
+                    isScrollingAwayFromTop -> consumed.y
+
+                    // re-enter the collapsible back into view only when overscrolling
+                    // from the top, by the amount overscrolled
+                    isOverscrollingAtTop -> available.y
+
+                    else -> null
+                }
+
+                deltaToApply?.let { delta ->
+                    val newTarget = (currentOffsetPx.value + delta).coerceIn(maxCollapseHeight, 0.0f)
+                    val newFraction = newTarget / maxCollapseHeight
+                    coroutineScope.launch {
+                        currentOffsetPx.snapTo(newTarget)
+                        offsetFraction.snapTo(newFraction)
+                    }
+                }
+
+                return super.onPostScroll(consumed, available, source)
+            }
+
+            override suspend fun onPostFling(consumed: Velocity, available: Velocity): Velocity {
+                val isOverfling = available.y > 0 && available.y > consumed.y
+
+                // when the user attempts to fling beyond the top of main content,
+                // we animate the collapsible back in smoothly
+                if (isOverfling) {
+                    val delta = available.y
+                    val newTarget = (currentOffsetPx.value + delta).coerceIn(maxCollapseHeight, 0.0f)
+                    val newFraction = newTarget / maxCollapseHeight
+                    coroutineScope.launch {
+                        // launching two coroutines allows the animations to occur simultaneously
+                        // rather than sequentially. it works fine but doesn't guarantee 100%
+                        // they're sync'ed like Transition<?> does, so would be nice to clean this up
+                        coroutineScope {
+                            launch {
+                                currentOffsetPx.animateTo(animationSpec = tween(250), targetValue = newTarget)
+                            }
+                            launch {
+                                offsetFraction.animateTo(animationSpec = tween(250), targetValue = newFraction)
+                            }
+                        }
+                    }
+                }
+                return super.onPostFling(consumed, available)
+            }
+        }
+}
