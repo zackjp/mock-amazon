1f70d1dfe481f22f554e2219da1a255a8858acf4
Zack P <zackjp87@gmail.com>
2025-10-05 16:00:59 -0400

    [App] Update ComingSoon screen to use Surface instead of background color

--- app/src/main/java/com/example/fakeamazon/base/ui/ComingSoonScreen.kt
+++ app/src/main/java/com/example/fakeamazon/base/ui/ComingSoonScreen.kt
@@ -1,30 +1,32 @@
 package com.example.fakeamazon.base.ui
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 
 @Composable
 fun ComingSoonScreen(modifier: Modifier = Modifier, title: String) {
-    Column(
-        modifier = modifier.background(Color.White),
-        verticalArrangement = Arrangement.Center,
-    ) {
-        Text(
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            style = MaterialTheme.typography.displayLarge,
-            text = title,
-        )
-        Text(
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            style = MaterialTheme.typography.bodyLarge,
-            text = "Coming soon.",
-        )
+    Surface(modifier = modifier) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+        ) {
+            Text(
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                style = MaterialTheme.typography.displayLarge,
+                text = title,
+            )
+            Text(
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                style = MaterialTheme.typography.bodyLarge,
+                text = "Coming soon.",
+            )
+        }
     }
 }
