59ec23bb2262a3498d79e9b11787361a18044164
Zack P <zackjp87@gmail.com>
2025-11-27 16:33:47 -0500

    [Cart] Refactor unit tests in CartFakeApiDataSourceTests
    - slightly cleaner, more understandable, and easier to maintain

--- app/src/test/java/com/example/fakeamazon/data/CartFakeApiDataSourceTest.kt
+++ app/src/test/java/com/example/fakeamazon/data/CartFakeApiDataSourceTest.kt
@@ -5,8 +5,9 @@ import com.example.fakeamazon.shared.model.ProductInfo
 import com.example.fakeamazon.shared.model.fakeInfo
 import com.example.fakeamazon.shared.model.toCartItem
 import io.kotest.matchers.collections.shouldContainExactly
-import io.kotest.matchers.collections.shouldContainOnly
+import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
 import io.kotest.matchers.shouldBe
+import io.mockk.coEvery
 import io.mockk.every
 import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
@@ -14,6 +15,14 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class CartFakeApiDataSourceTest {
+
+    private companion object {
+        private val PRODUCT_IN_CART = ProductInfo.fakeInfo(123)
+        private val PRODUCT_NOT_IN_CART = ProductInfo.fakeInfo(456)
+        private const val PRODUCT_IN_CART_QUANTITY = 7
+        private val EXISTING_CART_ITEMS =
+            listOf(PRODUCT_IN_CART.toCartItem(quantity = PRODUCT_IN_CART_QUANTITY))
+    }
 
     private val testDispatcherProvider = TestDispatcherProvider()
     private val testDispatcher = testDispatcherProvider.default
@@ -23,10 +32,20 @@ class CartFakeApiDataSourceTest {
 
     @BeforeEach
     fun setUp() {
+        listOf(PRODUCT_IN_CART, PRODUCT_NOT_IN_CART).forEach {
+            coEvery { productInMemoryDb.getProductById(it.id) } returns it
+        }
+
         dataSource = CartFakeApiDataSource(
             dispatcherProvider = testDispatcherProvider,
             productInMemoryDb = productInMemoryDb,
         )
+
+        runTest(testDispatcher) {
+            repeat(PRODUCT_IN_CART_QUANTITY) {
+                dataSource.addToCart(PRODUCT_IN_CART.id)
+            }
+        }
     }
 
     @Test
@@ -45,23 +64,11 @@ class CartFakeApiDataSourceTest {
     }
 
     @Test
-    fun removeByProductId_RemovesProductFromCart() = runTest(testDispatcher) {
-        val productInfo = ProductInfo.fakeInfo(123)
-        every { productInMemoryDb.getProductById(productInfo.id) } returns productInfo
-        repeat(3) { dataSource.addToCart(123) }
-        dataSource.getCartItems() shouldBe listOf(productInfo.toCartItem(3))
-
-        dataSource.removeByProductId(123)
-
-        dataSource.getCartItems() shouldBe emptyList()
-    }
-
-    @Test
-    fun getCartItems_WithDistinctValidProductIds_ReturnsCartItems() = runTest(testDispatcher) {
+    fun addToCart_WithDistinctValidProductIds_ReturnsCartItems() = runTest(testDispatcher) {
         val products = listOf(
-            ProductInfo.fakeInfo(123),
-            ProductInfo.fakeInfo(456),
-            ProductInfo.fakeInfo(789),
+            ProductInfo.fakeInfo(11),
+            ProductInfo.fakeInfo(13),
+            ProductInfo.fakeInfo(17),
         )
         products.forEach {
             every { productInMemoryDb.getProductById(it.id) } returns it
@@ -72,35 +79,46 @@ class CartFakeApiDataSourceTest {
         dataSource.addToCart(products[2].id)
 
         val cartItems = dataSource.getCartItems()
-        cartItems shouldContainOnly products.map { it.toCartItem(1) }
+        cartItems shouldContainExactlyInAnyOrder products.map { it.toCartItem(1) } + EXISTING_CART_ITEMS
     }
 
     @Test
-    fun getCartItems_WithDuplicateValidProductIds_ReturnsCartItemsWithQuantity() = runTest(testDispatcher) {
-        val products = listOf(
-            ProductInfo.fakeInfo(123),
-            ProductInfo.fakeInfo(456),
-        )
-        products.forEach {
-            every { productInMemoryDb.getProductById(it.id) } returns it
+    fun addToCart_WithRepeatedValidProductIds_ReturnsCartItemsWithQuantity() =
+        runTest(testDispatcher) {
+            val products = listOf(
+                ProductInfo.fakeInfo(11),
+                ProductInfo.fakeInfo(13),
+            )
+            products.forEach {
+                every { productInMemoryDb.getProductById(it.id) } returns it
+            }
+
+            repeat(3) { dataSource.addToCart(products[0].id) }
+            repeat(7) { dataSource.addToCart(products[1].id) }
+
+            val cartItems = dataSource.getCartItems()
+            cartItems shouldContainExactlyInAnyOrder listOf(
+                products[0].toCartItem(3),
+                products[1].toCartItem(7),
+            ) + EXISTING_CART_ITEMS
         }
 
-        repeat(3) { dataSource.addToCart(products[0].id) }
-        repeat(7) { dataSource.addToCart(products[1].id) }
+    @Test
+    fun removeByProductId_RemovesProductFromCart() = runTest(testDispatcher) {
+        val product = PRODUCT_IN_CART
+        dataSource.getCartItems() shouldBe listOf(product.toCartItem(PRODUCT_IN_CART_QUANTITY))
 
-        val cartItems = dataSource.getCartItems()
-        cartItems shouldContainOnly listOf(
-            products[0].toCartItem(3),
-            products[1].toCartItem(7),
-        )
+        dataSource.removeByProductId(product.id)
+
+        dataSource.getCartItems() shouldBe emptyList()
     }
 
     @Test
     fun getCartItems_ReturnsCartItemsInOrderWithMostRecentFirst() = runTest(testDispatcher) {
         val products = listOf(
-            ProductInfo.fakeInfo(123),
-            ProductInfo.fakeInfo(456),
-            ProductInfo.fakeInfo(789),
+            ProductInfo.fakeInfo(11),
+            ProductInfo.fakeInfo(13),
+            ProductInfo.fakeInfo(17),
         )
         products.forEach {
             every { productInMemoryDb.getProductById(it.id) } returns it
@@ -115,63 +133,60 @@ class CartFakeApiDataSourceTest {
             products[2].toCartItem(3),
             products[1].toCartItem(2),
             products[0].toCartItem(1),
-        )
+        ) + EXISTING_CART_ITEMS
     }
 
     @Test
-    fun getCartItems_WithValidProductIdThatBecameInvalidProductId_ReturnsNothing() = runTest(testDispatcher) {
-        val productInfo = ProductInfo.fakeInfo(123)
+    fun getCartItems_WithValidProductThatBecameInvalidProduct_ReturnsValidProductsOnly() =
+        runTest(testDispatcher) {
+            val productInfo = PRODUCT_NOT_IN_CART
 
-        every { productInMemoryDb.getProductById(productInfo.id) } returns productInfo
-        dataSource.addToCart(productInfo.id)
-        dataSource.getCartItems() shouldContainOnly listOf(productInfo.toCartItem(1))
+            dataSource.addToCart(productInfo.id)
+            dataSource.getCartItems() shouldContainExactly listOf(productInfo.toCartItem(1)) + EXISTING_CART_ITEMS
 
-        every { productInMemoryDb.getProductById(productInfo.id) } returns null
-        dataSource.getCartItems() shouldContainOnly emptyList()
-    }
+            every { productInMemoryDb.getProductById(productInfo.id) } returns null
+            dataSource.getCartItems() shouldContainExactly EXISTING_CART_ITEMS
+        }
 
     @Test
     fun decrementByProductId_WhenReachesZero_RemovesAsCartItem() = runTest(testDispatcher) {
-        val productInfo = ProductInfo.fakeInfo(123)
-        every { productInMemoryDb.getProductById(productInfo.id) } returns productInfo
+        val productInfo = PRODUCT_NOT_IN_CART
 
         dataSource.addToCart(productInfo.id)
         dataSource.addToCart(productInfo.id)
         dataSource.addToCart(productInfo.id)
 
-        dataSource.getCartItems() shouldContainExactly listOf(productInfo.toCartItem(3))
+        dataSource.getCartItems() shouldContainExactly listOf(productInfo.toCartItem(3)) + EXISTING_CART_ITEMS
 
         dataSource.decrementByProductId(productInfo.id)
         dataSource.decrementByProductId(productInfo.id)
         dataSource.decrementByProductId(productInfo.id)
 
-        dataSource.getCartItems() shouldBe emptyList()
+        dataSource.getCartItems() shouldBe EXISTING_CART_ITEMS
     }
 
     @Test
     fun decrementByProductId_WhenNonZero_DecrementsCartItemQuantity() = runTest(testDispatcher) {
-        val productInfo = ProductInfo.fakeInfo(123)
-        every { productInMemoryDb.getProductById(productInfo.id) } returns productInfo
+        val productInfo = PRODUCT_NOT_IN_CART
 
         dataSource.addToCart(productInfo.id)
         dataSource.addToCart(productInfo.id)
         dataSource.addToCart(productInfo.id)
 
-        dataSource.getCartItems() shouldContainExactly listOf(productInfo.toCartItem(3))
+        dataSource.getCartItems() shouldContainExactly listOf(productInfo.toCartItem(3)) + EXISTING_CART_ITEMS
 
         dataSource.decrementByProductId(productInfo.id)
 
-        dataSource.getCartItems() shouldContainExactly listOf(productInfo.toCartItem(2))
+        dataSource.getCartItems() shouldContainExactly listOf(productInfo.toCartItem(2)) + EXISTING_CART_ITEMS
     }
 
     @Test
     fun decrementByProductId_WhenNotAlreadyInCart_DoesNothing() = runTest(testDispatcher) {
-        val productInfo = ProductInfo.fakeInfo(123)
-        every { productInMemoryDb.getProductById(productInfo.id) } returns productInfo
+        val productInfo = PRODUCT_NOT_IN_CART
 
         dataSource.decrementByProductId(productInfo.id)
 
-        dataSource.getCartItems() shouldBe emptyList()
+        dataSource.getCartItems() shouldBe EXISTING_CART_ITEMS
     }
 
 }
