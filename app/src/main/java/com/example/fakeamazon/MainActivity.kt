d63783acd711167e303e828dea1907c2b9dac621
Zack P <zackjp87@gmail.com>
2025-11-29 18:12:19 -0500

    [App] Bug fix: make StatusBar legible even in dark mode (ie, avoid white text on white bg)
    - solution: set the status bar text color to always be dark
    - prior to this commit, in darkmode (eg at night), the status bar
      text color became white. this was a problem mainly on HomeScreen in
      which the AmazonTopAppBar background also turns white when scrolling

--- app/src/main/java/com/example/fakeamazon/MainActivity.kt
+++ app/src/main/java/com/example/fakeamazon/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 import com.example.fakeamazon.app.ui.App
 import com.example.fakeamazon.ui.theme.FakeAmazonTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -14,6 +16,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            WindowCompat.getInsetsController(
+                window,
+                LocalView.current
+            ).isAppearanceLightStatusBars = true
+
             FakeAmazonTheme {
                 App()
             }
